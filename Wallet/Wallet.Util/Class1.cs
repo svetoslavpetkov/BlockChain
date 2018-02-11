d5d671c626fc5acc83279c86b53aa1c277af3d8e
Svetoslav Petkov <svetoslav.petkov@gmail.com>
2018-02-11 17:37:15 +0200

    Initial commit

---
+++ Wallet/Wallet.Util/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Org.BouncyCastle.Asn1.X9;
+using System;
+
+namespace Wallet.Util
+{
+    public class Wallet
+    {
+        static readonly X9ECParameters curve = SecNamedCurves.GetByName("secp256k1");
+
+
+
+
+    }
+}
