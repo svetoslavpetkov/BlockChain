4db21b425fab456078cccdf6a05c4e5dd2862db3
Stefan <Stefan@LAPTOP-HBI7DU4G>
2018-02-11 08:57:02 -0800

    add transaction model

--- Node/Node/Node.Models/Transaction.cs
+++ Node/Node/Node.Models/Transaction.cs
@@ -6,7 +6,15 @@ namespace Node.Models
 {
     public class Transaction
     {
-        public string Sender { get; private set; }
-        public int MyProperty { get; private set; }
+        public string SenderAddress { get; private set; }
+        public string ReceiverAddress { get; private set; }
+        public decimal Amount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SenderPublicKey { get; set; }
+        public string[] Signature { get; private set; }
+        public string TransactionHash { get; private set; }
+
+        public int MinedInIndex { get; private set; }
+        public bool TranserSuccessful { get; private set; }
     }
 }
