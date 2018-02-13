59bee2b4de4c698fe25b3162db43dc7ae96aa467
svetoslavpetkov <svetoslav.petkov@gmail.com>
2018-02-13 15:05:57 +0200

    BlockChain.Core to handle signing aand verifying transactions

---
+++ BlockChain.Core/BlockChain.Core/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BlockChain.Core
+{
+    public class TransactionRaw
+    {
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+
+
+    public class Transaction : TransactionRaw
+    {
+        public string TransactionHash { get; set; }
+
+        public string SenderPublicKey { get; set; }
+
+        public string[] Signature { get; set; }
+    }
+}
