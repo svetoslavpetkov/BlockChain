9b06288e000fe5ae42a64ddab01529f32ba139b7
Stefan <stefosv@gmail.com>
2018-02-24 18:54:25 +0200

    node info wrong name

--- Node/Node/Node.Models/NodeInfo.cs
+++ Node/Node/Node.Models/NodeInfo.cs
@@ -4,12 +4,13 @@ namespace Node.Domain
 {
     public class NodeInfo
     {
-        public Guid Identifier { get; set; } = new Guid();
+        public Guid Identifier { get; set; }
         public string Name { get; private set; }
         public string About { get; private set; }
 
         public NodeInfo()
         {
+            Identifier = Guid.NewGuid(); 
             Name = $"Node {Identifier.ToString().Substring(6).ToUpper()}";
             About = "Node v1.0";    
         }
